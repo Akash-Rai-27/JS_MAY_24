6a225bc6522756af0e4bb820d4172079b30927a0
Akask-Rai-27 <spyeye118@gmail.com>
2024-06-02 20:24:24 +0530

    message

--- controlFlow/01control.js
+++ controlFlow/01control.js
@@ -10,3 +10,59 @@
 
 // }
 
+// switch
+
+const month = 5;
+
+switch(month){
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+
+/*
+
+falsy value
+
+false, 0, -0, bigint 0n, "" , null, undefined, NaN
+
+
+truty value
+
+"0", "false", " ", [] ,{} , function(){}
+
+
+*/
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("Object is empty")
+}
+
+// nullish coalescing operator (??) : null undefined
+
+let val1;
+val1 = undefined ?? "batman";
+console.log(val1);
+
+
+//terniary Operator
+
+
+// condition ? true : false
+
+const iceTeaPrice = 100;
+iceTeaPrice >=90 ? console.log("more than 90") : console.log("less than 90" );
---
+++ controlFlow/02iterations.js
@@ -0,0 +1,22 @@
+//for
+
+// for (let i = 0; i < 11; i++) {
+//     if(i%2 == 0){
+//         console.log(i);
+//     } 
+// }
+
+// while
+// let k = 10;
+// while(k>=0){
+//     console.log(`k value is : ${k}`);
+//     k--;
+// }
+
+// do - while loop
+let val = 1;
+do{
+console.log(val);
+val++;
+}
+while(val<10);
